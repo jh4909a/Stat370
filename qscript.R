b5c86a53d938ff1546de64e0850c904b599370dd
Jessica Hoag <jessicahoag@jessicas-mbp-4.american.edu>
2017-02-14 12:11:05 -0500

    Finished Homework 1

--- qscript.R
+++ qscript.R
@@ -4,7 +4,7 @@ quad <- function(x, a=0, b=0, c=0) {
 ##Zeros
 y <- quad(x)
 plot(x,y,'l')
-y2 <- quad(x,a=1,b=1,c=1)
+y2 <- quad(x,a=1)
 lines(x,y2)
-y3 <- quad(x,a=5,b=4,c=3)
 
+##new file, r markdown, source your code into it, code chunks for each plot, 6 options around 0, edit in script, but source to markdown

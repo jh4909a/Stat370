35efea7e949d60814340284157f9889934f5344d
Jessica Hoag <jessicahoag@jessicas-mbp-4.american.edu>
2017-02-17 12:06:24 -0500

    Updated

--- qscript.R
+++ qscript.R
@@ -2,9 +2,5 @@ quad <- function(x, a=0, b=0, c=0) {
   return(a*x^2+b*x+c)
 }
 ##Zeros
-y <- quad(x)
-plot(x,y,'l')
-y2 <- quad(x,a=1)
-lines(x,y2)
 
-##new file, r markdown, source your code into it, code chunks for each plot, 6 options around 0, edit in script, but source to markdown
+
